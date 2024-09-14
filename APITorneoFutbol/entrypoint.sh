5f00de04526ea1698068b49b826734583604f3c6
SomeRandomCatholic <christoamilcruz@gmail.com>
2024-09-14 16:21:55 -0600

    Arreglo de composer

--- APITorneoFutbol/entrypoint.sh
+++ APITorneoFutbol/entrypoint.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-if [ ! -f /var/www/html/database/database.sqlite ]; then
-touch /var/www/html/database/database.sqlite
-fi
-
+composer install
 php artisan migrate --force
 php artisan serve --host=0.0.0.0 --port=8000
 
